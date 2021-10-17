6d160eff733bdfa2c2dc84a9251e4e59be18725d
Rasmus Rudling <rasmusrudling@gmail.com>
2021-10-17 17:44:03 +0200

    first commit

---
+++ webpack/webpack.dev.js
@@ -0,0 +1,17 @@
+const webpack = require('webpack')
+const ReactRefreshWebpackPlugin = require('@pmmmwh/react-refresh-webpack-plugin')
+
+module.exports = {
+  mode: 'development',
+  devtool: 'cheap-module-source-map',
+  devServer: {
+    hot: true,
+    open: false,
+  },
+  plugins: [
+    new ReactRefreshWebpackPlugin(),
+    new webpack.DefinePlugin({
+      'process.env.name': JSON.stringify('Vishwas'),
+    }),
+  ],
+}
